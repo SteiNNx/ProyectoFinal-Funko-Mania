e6997a6edee33c50289bb9a874784d674f3fde72
Jorge Reyes <jorge_irving_15@hotmail.com>
2022-03-31 21:05:01 -0300

    ADD: Firebase Authentication

--- src/store/Products.js
+++ src/store/Products.js
@@ -1,3 +1,16 @@
+import {
+    collection,
+    addDoc,
+    query,
+    where,
+    getDocs,
+    onSnapshot,
+    doc,
+    deleteDoc,
+    updateDoc
+} from "firebase/firestore";
+import { db } from "@/plugins/firebase";
+
 /**
  * @description Modulo Productos, contiene state, mutations, actions y getters
  */
--- src/store/User.js
+++ src/store/User.js
@@ -1,10 +1,65 @@
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+} from "firebase/auth";
+
 /**
  * @description Modulo Usuario, contiene state, mutations, actions y getters
  */
 const User = {
-    state: () => ({}),
-    mutations: {},
-    actions: {},
+    state: () => ({
+        userLogin: null,
+        userMsjError: null,
+    }),
+    mutations: {
+        SET_USER_LOGIN(state, payload) {
+            state.userLogin = payload;
+        },
+        SET_USER_MSJ_ERROR(state, payload) {
+            state.userMsjError = payload;
+        },
+    },
+    actions: {
+        async registerUser({ commit }, userParam) {
+            const { email, password } = userParam;
+            const auth = getAuth();
+            await createUserWithEmailAndPassword(auth, email, password)
+                .then(() => {
+                    commit('SET_USER_MSJ_ERROR', null);
+                })
+                .catch((error) => {
+                    console.log(error);
+                    commit('SET_USER_MSJ_ERROR', errorCodeToStringLabelFirebase(error.code));
+                });
+        },
+        async loginUser({ commit }, userParam) {
+            const { email, password } = userParam;
+            const auth = getAuth();
+            await signInWithEmailAndPassword(auth, email, password)
+                .then(() => {
+                    commit('SET_USER_MSJ_ERROR', null);
+                })
+                .catch((error) => {
+                    console.log(error);
+                    commit('SET_USER_MSJ_ERROR', errorCodeToStringLabelFirebase(error.code));
+                });;
+        },
+        async logoutUser({ commit }) {
+            await getAuth()
+                .signOut()
+                .then(() => {
+                    commit('SET_USER_LOGIN', null);
+                });
+        },
+        getUserLogin({ commit }) {
+            getAuth().onAuthStateChanged((user) => {
+                if (user) {
+                    commit('SET_USER_LOGIN', user.email);
+                }
+            });
+        },
+    },
     getters: {}
 };
 
