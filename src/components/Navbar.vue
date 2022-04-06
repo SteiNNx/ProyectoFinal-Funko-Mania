0ae035bac3e5afb421b94f65759c6e62926ab1b9
Natalia <nataliagonzalez.1802@gmail.com>
2022-04-06 15:31:04 -0400

    imagen y navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <b-navbar class="nav p-2" toggleable="lg" type="dark" variant="">
+      <b-navbar-brand href="/">FUNKO-MANIA</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item to="/">Home</b-nav-item>
+          <b-nav-item to="/administracion">Administracion</b-nav-item>
+          <b-nav-item to="/login">Login</b-nav-item>
+          <b-nav-item to="/registrar">Registrar</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+      </b-collapse>
+      <div v-if="userLogin != null">
+        <span @click="logout" v-b-tooltip.hover title="Exit"
+          ><b-avatar icon="door-closed"></b-avatar
+        ></span>
+        <span>{{ userLogin }}</span>
+      </div>
+      <span v-else @click="ingresar">Ingresar</span>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+.nav {
+  background-color: #00A4CD;
+}
+span {
+  color: #fff;
+}
+</style>
