dbd48042ba772dc6c4e6b41dc690d9447bee26aa
Natalia <nataliagonzalez.1802@gmail.com>
2022-04-07 20:50:47 -0400

    categorias

--- src/views/Categorias.vue
+++ src/views/Categorias.vue
@@ -1,19 +1,82 @@
 <template id="Categorias">
 <div>
 <b-card
-    title="Card Title"
-    img-src="https://picsum.photos/600/300/?image=25"
-    img-alt="Image"
+    title="ROCK"
+    img-src="../assets/img/rock.jpg"
+    img-alt="rock"
     img-top
     tag="article"
     style="max-width: 20rem;"
     class="mb-2"
 >
-    <b-card-text>
-    Some quick example text to build on the card title and make up the bulk of the card's content.
-    </b-card-text>
+    <b-button href="#" variant="primary">Ver mas</b-button>
+</b-card>
 
-    <b-button href="#" variant="primary">Go somewhere</b-button>
+<b-card
+    title="Anime"
+    img-src="../assets/img/anime.jpg"
+    img-alt="anime"
+    img-top
+    tag="article"
+    style="max-width: 20rem;"
+    class="mb-2"
+>
+    <b-button href="#" variant="primary">Ver mas</b-button>
+</b-card>
+<b-card
+    title="BOOK"
+    img-src="../assets/img/book.jpg"
+    img-alt="book"
+    img-top
+    tag="article"
+    style="max-width: 20rem;"
+    class="mb-2"
+>
+    <b-button href="#" variant="primary">Ver mas</b-button>
+</b-card>
+<b-card
+    title="COMICS"
+    img-src="../assets/img/comics.jpg"
+    img-alt="comics"
+    img-top
+    tag="article"
+    style="max-width: 20rem;"
+    class="mb-2"
+>
+    <b-button href="#" variant="primary">Ver mas</b-button>
+</b-card>
+<b-card
+    title="DISNEY"
+    img-src="../assets/img/disney.jpg"
+    img-alt="disney"
+    img-top
+    tag="article"
+    style="max-width: 20rem;"
+    class="mb-2"
+>
+    <b-button href="#" variant="primary">Ver mas</b-button>
+</b-card>
+<b-card
+    title="HEROES"
+    img-src="../assets/img/heroe.jpg"
+    img-alt="heroes"
+    img-top
+    tag="article"
+    style="max-width: 20rem;"
+    class="mb-2"
+>
+    <b-button href="#" variant="primary">Ver mas</b-button>
+</b-card>
+<b-card
+    title="TELEVISIÓN"
+    img-src="../assets/img/television.jpg"
+    img-alt="television"
+    img-top
+    tag="article"
+    style="max-width: 20rem;"
+    class="mb-2"
+>
+    <b-button href="#" variant="primary">Ver mas</b-button>
 </b-card>
 </div>
 </template>
