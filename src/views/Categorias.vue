6c57b029165b43c5e476870f9b9c3d05b30b4bd8
Natalia <nataliagonzalez.1802@gmail.com>
2022-04-07 21:07:41 -0400

    boton texto

--- src/views/Categorias.vue
+++ src/views/Categorias.vue
@@ -1,80 +1,66 @@
 <template id="Categorias">
 <div>
-<b-card
-    title="ROCK"
+<b-card text-variant="black" header="ROCK" class="text-center"
     img-src="../assets/img/rock.jpg"
     img-alt="rock"
     img-top
     tag="article"
     style="max-width: 20rem;"
-    class="mb-2"
 >
     <b-button href="#" variant="primary">Ver mas</b-button>
 </b-card>
 
-<b-card
-    title="Anime"
+<b-card text-variant="black" header="ANIME" class="text-center"
     img-src="../assets/img/anime.jpg"
     img-alt="anime"
     img-top
     tag="article"
     style="max-width: 20rem;"
-    class="mb-2"
 >
     <b-button href="#" variant="primary">Ver mas</b-button>
 </b-card>
-<b-card
-    title="BOOK"
+<b-card text-variant="black" header="BOOK" class="text-center"
     img-src="../assets/img/book.jpg"
     img-alt="book"
     img-top
     tag="article"
     style="max-width: 20rem;"
-    class="mb-2"
 >
     <b-button href="#" variant="primary">Ver mas</b-button>
 </b-card>
-<b-card
-    title="COMICS"
+<b-card text-variant="black" header="COMICS" class="text-center"
     img-src="../assets/img/comics.jpg"
     img-alt="comics"
     img-top
     tag="article"
     style="max-width: 20rem;"
-    class="mb-2"
 >
     <b-button href="#" variant="primary">Ver mas</b-button>
 </b-card>
-<b-card
-    title="DISNEY"
+<b-card text-variant="black" header="DISNEY" class="text-center"
     img-src="../assets/img/disney.jpg"
     img-alt="disney"
     img-top
     tag="article"
     style="max-width: 20rem;"
-    class="mb-2"
 >
     <b-button href="#" variant="primary">Ver mas</b-button>
 </b-card>
-<b-card
-    title="HEROES"
+<b-card text-variant="black" header="HEROES" class="text-center"
     img-src="../assets/img/heroe.jpg"
     img-alt="heroes"
     img-top
     tag="article"
     style="max-width: 20rem;"
-    class="mb-2"
 >
     <b-button href="#" variant="primary">Ver mas</b-button>
 </b-card>
-<b-card
-    title="TELEVISIÓN"
+<b-card text-variant="black" header="TELEVISIÓN" class="text-center"
     img-src="../assets/img/television.jpg"
     img-alt="television"
     img-top
     tag="article"
     style="max-width: 20rem;"
-    class="mb-2"
 >
     <b-button href="#" variant="primary">Ver mas</b-button>
 </b-card>
