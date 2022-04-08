21628f56f2281cb10745d88f9ad00f79115f4c30
Jorge Reyes <jorge_irving_15@hotmail.com>
2022-04-07 21:47:43 -0400

    ADD: Navbar, NavbarLogin, Carousel fixed, ad style

---
+++ src/components/Carousel.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="funko-carousel">
+    <b-carousel
+      id="carousel-1"
+      v-model="slide"
+      :interval="3000"
+      indicators
+      background="#ccc"
+      img-width="1024"
+      img-height="480"
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd"
+    >
+      <b-carousel-slide class="funko-carousel__img" img-src="@/assets/img/banner1.jpg">
+      </b-carousel-slide>
+
+      <b-carousel-slide class="funko-carousel__img" img-src="@/assets/img/banner2.jpg">
+      </b-carousel-slide>
+
+      <b-carousel-slide
+        class="funko-carousel__img"
+        img-src="@/assets/img/banner3.jpg"
+      ></b-carousel-slide>
+
+      <b-carousel-slide class="funko-carousel__img" img-src="@/assets/img/banner4.jpg">
+      </b-carousel-slide>
+    </b-carousel>
+  </div>
+</template>
+<script>
+export default {
+  name: "Carousel",
+  data() {
+    return {
+      slide: 0,
+      sliding: null,
+    };
+  },
+  methods: {
+    onSlideStart() {
+      this.sliding = true;
+    },
+    onSlideEnd() {
+      this.sliding = false;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.funko-carousel {
+  height: 100%;
+  &__img {
+    width: 100%;
+    height: 30rem;
+    object-fit: cover;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,43 +1,14 @@
-<template id="Home">
-  <div>
-    <b-carousel
-      id="carousel-1"
-      v-model="slide"
-      :interval="4000"
-      controls
-      indicators
-      background="#ababab"
-      img-width="1024"
-      img-height="300"
-      style="text-shadow: 1px 1px 2px #333;"
-      @sliding-start="onSlideStart"
-      @sliding-end="onSlideEnd"
-    >
-      <b-carousel-slide
-        img-src="../assets/img/banner1.jpg"
-      ></b-carousel-slide>
-
-      <b-carousel-slide img-src="../assets/img/banner2.jpg">
-      </b-carousel-slide>
-
-      <b-carousel-slide img-src="../assets/img/banner3.jpg"></b-carousel-slide>
-
-      <b-carousel-slide>
-        <template #img>
-          <img
-            class="d-block img-fluid w-100"
-            src="../assets/img/banner4.jpg"
-            alt="image slot"
-          >
-        </template>
-      </b-carousel-slide>
-
-      
-    </b-carousel>
-
-    <p class="mt-4">
-      Slide #: {{ slide }}<br>
-      Sliding: {{ sliding }}
-    </p>
-  </div>
+<template>
+  <Carousel />
 </template>
+
+<script>
+import Carousel from "@/components/Carousel";
+
+export default {
+  name: "Home",
+  components: {
+    Carousel,
+  },
+};
+</script>
