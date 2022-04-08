366ca1ba32fadc1a5f6040a93de1bbf71d2ed347
Natalia <nataliagonzalez.1802@gmail.com>
2022-04-07 22:33:29 -0400

    favoritos

--- src/views/Favoritos.vue
+++ src/views/Favoritos.vue
@@ -1,4 +1,24 @@
-<template></template>
+<template id="favoritos">
+<b-container fluid class="p-4 bg-dark">
+  <b-row>
+    <b-col>
+      <b-img thumbnail fluid src="../assets/img/images (1).jpeg" alt="Image 1"></b-img>
+    </b-col>
+    <b-col>
+      <b-img thumbnail fluid src="../assets/img/avatar.jpeg" alt="Image 2"></b-img>
+    </b-col>
+    <b-col>
+      <b-img thumbnail fluid src="../assets/img/images (1).jpeg" alt="Image 3"></b-img>
+    </b-col>
+    <b-col>
+      <b-img thumbnail fluid src="../assets/img/images (1).jpeg" alt="Image 3"></b-img>
+    </b-col>
+    <b-col>
+      <b-img thumbnail fluid src="../assets/img/images (1).jpeg" alt="Image 3"></b-img>
+    </b-col>
+  </b-row>
+</b-container>
+</template>
 
 <script>
 export default {};
