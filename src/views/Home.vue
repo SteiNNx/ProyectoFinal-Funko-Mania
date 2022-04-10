2056b1dbd769c5500a82fa3761665a5e843b9491
Jorge Reyes <jorge_irving_15@hotmail.com>
2022-04-10 13:51:59 -0400

    UPDATE: Home se hace responsivo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,176 @@
 <template>
   <div>
-  <Carousel />
-  <HomeParte1/>
-  <InformacionExtra/>
+    <Carousel />
+    <div class="container">
+      <div class="row">
+        <div class="col-12 py-5">
+          <h1 class="funko-home-title">NO TE QUEDES SIN TU COLECCIÓN DE FUNKO POP!!</h1>
+        </div>
+        <div class="col-12">
+          <div class="row">
+            <div class="col-sm-12 col-md-6 col-lg-6 my-auto">
+              <p>
+                Bienvenidos al mundo de Funko Pop, aqui podras encontrar la historia,
+                datos curiosos, intercambiar y comprar para completar tu colección de uno
+                de los mayores fenómenos de coleccionistas modernos.
+              </p>
+              <p>
+                Quisque pretium hendrerit metus vel placerat. Aenean semper condimentum
+                magna, a pharetra nisi imperdiet non. Curabitur sodales, ligula quis
+                semper pellentesque, mi tellus vestibulum nisi, id tincidunt nisi est
+                fringilla massa. Nunc bibendum mi vel tempor lobortis. Phasellus arcu dui,
+                lacinia non bibendum quis, aliquet sit amet enim. Vivamus dignissim
+                facilisis aliquam. Etiam odio mauris, pulvinar in porttitor at,
+                pellentesque eu purus. Nam dapibus rhoncus justo et egestas. Sed sagittis
+                leo a pulvinar in porttitor at, pellentesque eu purus. Nam dapibus rhoncus
+                justo et egestas. Sed sagittis leo a
+              </p>
+            </div>
+            <div class="col-sm-12 col-md-6 col-lg-6 my-auto">
+              <img class="img-fluid" src="@/assets/img/inicio.jpg" />
+            </div>
+          </div>
+        </div>
+        <div class="col-12 py-5">
+          <h1 class="funko-home-title">LA HISTORIA DE LOS FUNKO POP</h1>
+        </div>
+        <div class="col-12">
+          <div class="row">
+            <div class="col-sm-12 col-md-6 col-lg-6 my-auto">
+              <p>
+                En 1998, Mike Becker, diseñador de camisetas y buscador de juguetes raros,
+                tenía entre ceja y ceja el muñeco-hucha con el famoso Big Boy de la cadena
+                de 'fast food' americana. El problema es que todo aquel que se la quería
+                vender pedía una millonada por ella. Ante tal panorama decidió fabricar su
+                propia versión en China por mucho menos dinero. Al poco tiempo nacía
+                Funko. Becker empezó con 35.000 dólares de su propio bolsillo trabajando
+                desde su casa y su primer muñequito, como no podía ser de otra manera, fue
+                el Big Boy que había estado buscando durante tanto tiempo, esta vez en
+                versión muñeco sin hucha. Acto seguido fue a por su primera línea
+                bautizada con el nombre de Wacky Wobblers (algo así como los “locos que se
+                tambalean”) cuyos protagonistas eran figuras animadas como Popeye, Dick
+                Tracy o personajes de las marcas de cereales de General Mills. Becker
+                empezó con 35.000 dólares de su propio bolsillo trabajando desde su casa y
+                su primer muñequito, como no podía ser de otra manera, fue el Big Boy que
+                había estado buscando durante tanto tiempo, esta vez en versión muñeco sin
+                hucha. Acto seguido fue a por su primera línea bautizada con el nombre de
+                Wacky Wobblers (algo así como los “locos que se tambalean”) cuyos
+                protagonistas eran figuras animadas como Popeye, Dick Tracy o personajes
+                de las marcas de cereales de General Mills.
+              </p>
+            </div>
+            <div class="col-sm-12 col-md-6 col-lg-6">
+              <img class="img-fluid" src="@/assets/img/BOY.jpg" />
+            </div>
+          </div>
+        </div>
+        <div class="col-12">
+          <div class="row">
+            <div class="col-sm-12 col-md-4 col-lg-4">
+              <div class="row">
+                <div class="col-12">
+                  <img class="img-fluid funko-home-sub-img" src="@/assets/img/lupa.png" />
+                </div>
+                <div class="col-12">
+                  <h2 class="mt-5 funko-home-sub-title">Funkos mas buscados</h2>
+                </div>
+                <div class="col-12">
+                  <p>
+                    Quisque faucibus sem at justo consectetur, a dictum ante rutrum. Proin
+                    in ipsum tempor, pharetra quam vitae, tristique ex. Sed molestie eget
+                    tellus eu scelerisque. Ut pretium arcu ac augue egestas, ut
+                    pellentesque lectus hendrerit. Vivamus interdum lorem sed facilisis
+                    volutpat. Donec varius arcu ac condimentum elementum.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-12 col-md-4 col-lg-4">
+              <div class="row">
+                <div class="col-12">
+                  <img
+                    class="img-fluid funko-home-sub-img"
+                    src="@/assets/img/curiosidades.png"
+                  />
+                </div>
+                <div class="col-12">
+                  <h2 class="mt-5 funko-home-sub-title">Curiosidades</h2>
+                </div>
+                <div class="col-12">
+                  <p>
+                    Quisque faucibus sem at justo consectetur, a dictum ante rutrum. Proin
+                    in ipsum tempor, pharetra quam vitae, tristique ex. Sed molestie eget
+                    tellus eu scelerisque. Ut pretium arcu ac augue egestas, ut
+                    pellentesque lectus hendrerit. Vivamus interdum lorem sed facilisis
+                    volutpat. Donec varius arcu ac condimentum elementum.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-12 col-md-4 col-lg-4">
+              <div class="row">
+                <div class="col-12">
+                  <img
+                    class="img-fluid funko-home-sub-img"
+                    src="@/assets/img/promociones.png"
+                  />
+                </div>
+                <div class="col-12">
+                  <h2 class="mt-5 funko-home-sub-title">Promociones</h2>
+                </div>
+                <div class="col-12">
+                  <p>
+                    Quisque faucibus sem at justo consectetur, a dictum ante rutrum. Proin
+                    in ipsum tempor, pharetra quam vitae, tristique ex. Sed molestie eget
+                    tellus eu scelerisque. Ut pretium arcu ac augue egestas, ut
+                    pellentesque lectus hendrerit. Vivamus interdum lorem sed facilisis
+                    volutpat. Donec varius arcu ac condimentum elementum.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-
 <script>
 import Carousel from "@/components/Carousel";
-import HomeParte1 from '@/views/HomeParte1';
-import InformacionExtra from '@/views/InformacionExtra';
-
-
 export default {
   name: "Home",
   components: {
     Carousel,
-    HomeParte1,
-    InformacionExtra,
-
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.funko-home-title {
+  text-transform: uppercase;
+  text-align: center;
+  color: $celeste;
+  font-family: $bouncy;
+}
+
+.funko-home-sub-title {
+  color: $celeste;
+  font-family: $bouncy;
+  font-size: $font-size-md;
+  text-align: center;
+  text-transform: uppercase;
+  min-height: 4rem;
+}
+
+.funko-home-sub-img {
+  align-items: center;
+  display: flex;
+  margin: auto;
+  max-height: 10rem;
+}
+
+p {
+  text-align: justify;
+}
+</style>
---
+++ src/views/NotFound.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container py-5">
+    <div class="jumbotron jumbotron-fluid bg-white">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 text-center">
+            <h1 class="funko-not-found-title">Page Not Found</h1>
+            <p class="funko-not-found-text">No pudimos encontrar la página que buscas.</p>
+          </div>
+          <div class="col-12 d-flex justify-content-center">
+            <img src="@/assets/img/not-found.gif" alt="john-travolta-meme" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.funko-not-found-title {
+  color: $celeste-oscuro;
+  font-family: $bouncy;
+  font-size: $font-size-xl;
+  font-weight: 500;
+  line-height: 1.2;
+}
+.funko-not-found-text {
+  font-family: $bouncy;
+  font-size: $font-size-sm;
+  font-weight: 500;
+  line-height: 1.2;
+}
+
+img {
+  @include breakpoint("Celular") {
+    height: 224px;
+    width: 216px;
+  }
+}
+</style>
