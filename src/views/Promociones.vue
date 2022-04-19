7b57295581ac7603df12234605cc04d49466d080
Natalia <nataliagonzalez.1802@gmail.com>
2022-04-18 22:24:02 -0400

    pagina promociones

--- src/views/Promociones.vue
+++ src/views/Promociones.vue
@@ -2,20 +2,91 @@
   <div>
     <b-card
       overlay
-      img-src="../assets/img/banner8.png"
-      img-alt="banner8"
+      img-src="../assets/img/BANNER7.png"
+      img-alt="banner7"
       text-variant="white"
       title="PROMOCIONES FUNKO"
-      sub-title="Aqui encontraras las mejores ofertas"
+      sub-title="Las mejores ofertas !!"
     >
     </b-card>
+    <div class="col-12 py-5">
+      <div class="row mx-5">
+        <div class="col-sm-12 col-md-6 col-lg-3">
+          <b-card
+            title="Funko POP! Caja Misteriosa"
+            img-src="@/assets/img/box.png"
+            img-alt="Image"
+            img-top
+            tag="article"
+            style="max-width: 20rem"
+            class="mb-2"
+          >
+            <b-card-text> $10.000 </b-card-text>
+
+            <b-button href="#" variant="primary" class="btn-funko-primary w-100"
+              >Comprar</b-button
+            >
+          </b-card>
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-3">
+          <b-card
+            title="Funko POP! Caja Misteriosa"
+            img-src="@/assets/img/box2.png"
+            img-alt="Image"
+            img-top
+            tag="article"
+            style="max-width: 20rem"
+            class="mb-2"
+          >
+            <b-card-text> $19.000 </b-card-text>
+
+            <b-button href="#" variant="primary" class="btn-funko-primary w-100"
+              >Comprar</b-button
+            >
+          </b-card>
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-3">
+          <b-card
+            title="Funko POP! Caja Misteriosa"
+            img-src="@/assets/img/box3.png"
+            img-alt="Image"
+            img-top
+            tag="article"
+            style="max-width: 20rem"
+            class="mb-2"
+          >
+            <b-card-text> $28.000 </b-card-text>
+
+            <b-button href="#" variant="primary" class="btn-funko-primary w-100"
+              >Comprar</b-button
+            >
+          </b-card>
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-3">
+          <b-card
+            title="Funko POP! Caja Misteriosa"
+            img-src="@/assets/img/box4.png"
+            img-alt="Image"
+            img-top
+            tag="article"
+            style="max-width: 20rem"
+            class="mb-2"
+          >
+            <b-card-text> $37.000 </b-card-text>
+
+            <b-button href="#" variant="primary" class="btn-funko-primary w-100"
+              >Comprar</b-button
+            >
+          </b-card>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <style lang="scss" scoped>
 * {
   font-family: $akshar;
+align-content: center;
 }
-img {
-  height: 33rem;
-}
+
 </style>
