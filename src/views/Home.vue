cc9e16b27ee0ead38f82e5fbf8bed0fe1c31439b
Jorge Reyes <jorge_irving_15@hotmail.com>
2022-04-18 20:25:17 -0400

    FIX: Style css home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,19 +4,16 @@
     <div class="container-fluid">
       <div class="row">
         <div class="col-12 pt-5 pb-3">
-          <h1 class="funko-home-title">
-            NO TE QUEDES SIN TU COLECCIÓN DE FUNKO POP!!
-          </h1>
+          <h1 class="funko-home-title">NO TE QUEDES SIN TU COLECCIÓN DE FUNKO POP!!</h1>
           <hr />
         </div>
         <div class="col-12">
           <div class="row">
             <div class="col-sm-12 col-md-12 col-lg-12">
               <p class="funko-home-bienvenida mb-5 pl-5 ml-5 mr-5 pr-5">
-                Bienvenidos al mundo de Funko Pop, te contaremos la historia,
-                datos curiosos, podras intercambiar tus funkos y comprar para
-                completar tu colección de uno de los mayores fenómenos de
-                coleccionistas modernos.
+                Bienvenidos al mundo de Funko Pop, te contaremos la historia, datos
+                curiosos, podras intercambiar tus funkos y comprar para completar tu
+                colección de uno de los mayores fenómenos de coleccionistas modernos.
               </p>
             </div>
             <div class="col-sm-12 col-md-12 col-lg-12 d-flex">
@@ -38,29 +35,26 @@
         <div class="col-10 texto-imagen">
           <div class="row">
             <div class="col-sm-12 col-md-12 col-lg-6">
-              <p class="funko-home-historia mb-5">
-                En 1998, Mike Becker, diseñador de camisetas y buscador de
-                juguetes raros, tenía entre ceja y ceja el muñeco-hucha con el
-                famoso Big Boy de la cadena de 'fast food' americana. El
-                problema es que todo aquel que se la quería vender pedía una
-                millonada por ella. Ante tal panorama decidió fabricar su propia
-                versión en China por mucho menos dinero. Al poco tiempo nacía
-                Funko. Becker empezó con 35.000 dólares de su propio bolsillo
-                trabajando desde su casa y su primer muñequito, como no podía
-                ser de otra manera, fue el Big Boy que había estado buscando
-                durante tanto tiempo, esta vez en versión muñeco sin hucha. Acto
-                seguido fue a por su primera línea bautizada con el nombre de
-                Wacky Wobblers (algo así como los “locos que se tambalean”)
-                cuyos protagonistas eran figuras animadas como Popeye, Dick
-                Tracy o personajes de las marcas de cereales de General Mills.
-                Becker empezó con 35.000 dólares de su propio bolsillo
-                trabajando desde su casa y su primer muñequito, como no podía
-                ser de otra manera, fue el Big Boy que había estado buscando
-                durante tanto tiempo, esta vez en versión muñeco sin hucha. Acto
-                seguido fue a por su primera línea bautizada con el nombre de
-                Wacky Wobblers (algo así como los “locos que se tambalean”)
-                cuyos protagonistas eran figuras animadas como Popeye, Dick
-                Tracy o personajes de las marcas de cereales de General Mills.
+              <p class="mx-5 mb-5">
+                En 1998, Mike Becker, diseñador de camisetas y buscador de juguetes raros,
+                tenía entre ceja y ceja el muñeco-hucha con el famoso Big Boy de la cadena
+                de 'fast food' americana. El problema es que todo aquel que se la quería
+                vender pedía una millonada por ella. Ante tal panorama decidió fabricar su
+                propia versión en China por mucho menos dinero. Al poco tiempo nacía
+                Funko. Becker empezó con 35.000 dólares de su propio bolsillo trabajando
+                desde su casa y su primer muñequito, como no podía ser de otra manera, fue
+                el Big Boy que había estado buscando durante tanto tiempo, esta vez en
+                versión muñeco sin hucha. Acto seguido fue a por su primera línea
+                bautizada con el nombre de Wacky Wobblers (algo así como los “locos que se
+                tambalean”) cuyos protagonistas eran figuras animadas como Popeye, Dick
+                Tracy o personajes de las marcas de cereales de General Mills. Becker
+                empezó con 35.000 dólares de su propio bolsillo trabajando desde su casa y
+                su primer muñequito, como no podía ser de otra manera, fue el Big Boy que
+                había estado buscando durante tanto tiempo, esta vez en versión muñeco sin
+                hucha. Acto seguido fue a por su primera línea bautizada con el nombre de
+                Wacky Wobblers (algo así como los “locos que se tambalean”) cuyos
+                protagonistas eran figuras animadas como Popeye, Dick Tracy o personajes
+                de las marcas de cereales de General Mills.
               </p>
             </div>
             <div class="col-sm-12 col-md-6 col-lg-6 mb-5">
@@ -86,10 +80,7 @@
               >
                 <b-card-text> $12.990 </b-card-text>
 
-                <b-button
-                  href="#"
-                  variant="primary"
-                  class="btn-funko-primary w-100"
+                <b-button href="#" variant="primary" class="btn-funko-primary w-100"
                   >Comprar</b-button
                 >
               </b-card>
@@ -106,10 +97,7 @@
               >
                 <b-card-text> $12.990 </b-card-text>
 
-                <b-button
-                  href="#"
-                  variant="primary"
-                  class="btn-funko-primary w-100"
+                <b-button href="#" variant="primary" class="btn-funko-primary w-100"
                   >Comprar</b-button
                 >
               </b-card>
@@ -126,10 +114,7 @@
               >
                 <b-card-text> $12.990 </b-card-text>
 
-                <b-button
-                  href="#"
-                  variant="primary"
-                  class="btn-funko-primary w-100"
+                <b-button href="#" variant="primary" class="btn-funko-primary w-100"
                   >Comprar</b-button
                 >
               </b-card>
@@ -146,10 +131,7 @@
               >
                 <b-card-text> $12.990 </b-card-text>
 
-                <b-button
-                  href="#"
-                  variant="primary"
-                  class="btn-funko-primary w-100"
+                <b-button href="#" variant="primary" class="btn-funko-primary w-100"
                   >Comprar</b-button
                 >
               </b-card>
@@ -166,10 +148,7 @@
               >
                 <b-card-text> $12.990 </b-card-text>
 
-                <b-button
-                  href="#"
-                  variant="primary"
-                  class="btn-funko-primary w-100"
+                <b-button href="#" variant="primary" class="btn-funko-primary w-100"
                   >Comprar</b-button
                 >
               </b-card>
@@ -179,91 +158,86 @@
           <hr />
           <br />
         </div>
-        <div class="col-12 ml-5 pl-5 pr-5 mr-5">
+        <div class="col-12 mb-4">
           <div class="row">
-            <div class="col-sm-12 col-md-4 col-lg-4">
+            <div class="col-sm-12 col-md-4 col-lg-4 p-5">
               <div class="row">
-                <div class="col-9">
-                  <img
-                    class="img-fluid funko-home-sub-img"
-                    src="@/assets/img/lupa.png"
-                  />
+                <div class="col-12">
+                  <img class="img-fluid funko-home-sub-img" src="@/assets/img/lupa.png" />
                 </div>
-                <div class="col-9">
+                <div class="col-12">
                   <h2 class="mt-4 funko-home-sub-title">Funkos mas buscados</h2>
                 </div>
-                <div class="col-9">
+                <div class="col-12">
                   <p>
-                    Hay una gran cantidad de Funkos, pero entre todos estos, hay
-                    algunos que destacan, ya sea por el personaje al que
-                    representa o por su apariencia atractiva. Éstos Funko Pop se
-                    convierten en los Funko Pop más buscados a lo largo del
-                    tiempo, una consecuencia directa de su calidad. Aqui podras
-                    conocer algunos de los Funko Pop más buscados.
+                    Hay una gran cantidad de Funkos, pero entre todos estos, hay algunos
+                    que destacan, ya sea por el personaje al que representa o por su
+                    apariencia atractiva. Éstos Funko Pop se convierten en los Funko Pop
+                    más buscados a lo largo del tiempo, una consecuencia directa de su
+                    calidad. Aqui podras conocer algunos de los Funko Pop más buscados.
                   </p>
                   <b-button
                     href="/buscados"
                     variant="primary"
-                    class="btn-funko-primary w-100 mb-5"
+                    class="btn-funko-primary w-100"
                     >Ver más</b-button
                   >
                 </div>
               </div>
             </div>
-            <div class="col-sm-12 col-md-4 col-lg-4">
+            <div class="col-sm-12 col-md-4 col-lg-4 p-5">
               <div class="row">
-                <div class="col-9">
+                <div class="col-12">
                   <img
                     class="img-fluid funko-home-sub-img"
                     src="@/assets/img/curiosidades.png"
                   />
                 </div>
-                <div class="col-9">
+                <div class="col-12">
                   <h2 class="mt-4 funko-home-sub-title">Curiosidades</h2>
                 </div>
-                <div class="col-9">
+                <div class="col-12">
                   <p>
-                    En Estados Unidos se sirven a la venta cereales Funko Pop
-                    basados en películas como It, Bitelchús, Gremlins o
-                    Pesadilla en Elm Street. Cada caja tiene un diseño único en
-                    función de la película que represente, así como un color
-                    diferente en los cereales. Además, cada caja cuenta con su
-                    propia mini figura Funko Pop. ¡Toda una delicia para los
+                    En Estados Unidos se sirven a la venta cereales Funko Pop basados en
+                    películas como It, Bitelchús, Gremlins o Pesadilla en Elm Street. Cada
+                    caja tiene un diseño único en función de la película que represente,
+                    así como un color diferente en los cereales. Además, cada caja cuenta
+                    con su propia mini figura Funko Pop. ¡Toda una delicia para los
                     amantes del coleccionismo!
                   </p>
                   <b-button
                     href="/curiosidades"
                     variant="primary"
-                    class="btn-funko-primary w-100 mb-5"
+                    class="btn-funko-primary w-100"
                     >Ver más</b-button
                   >
                 </div>
               </div>
             </div>
-            <div class="col-sm-12 col-md-4 col-lg-4">
+            <div class="col-sm-12 col-md-4 col-lg-4 p-5">
               <div class="row">
-                <div class="col-9">
+                <div class="col-12">
                   <img
                     class="img-fluid funko-home-sub-img"
                     src="@/assets/img/promociones.png"
                   />
                 </div>
-                <div class="col-9">
+                <div class="col-12">
                   <h2 class="mt-4 funko-home-sub-title">Promociones</h2>
                 </div>
-                <div class="col-9">
+                <div class="col-12">
                   <p>
-                    Encuentra cientos de Funko POP de tus series, películas y
-                    dibujos animados favoritos en su tamaño regular o en la
-                    versión pocket para que siempre los lleves contigo y así
-                    nunca dejes de lucir tu colección en cualquier parte. Compra
-                    tu colección de Funko POP tv, Marvel, Disney, Anime y más en
-                    un solo lugar. Te mostraremos todas nuestras promociones.
+                    Encuentra cientos de Funko POP de tus series, películas y dibujos
+                    animados favoritos en su tamaño regular o en la versión pocket para
+                    que siempre los lleves contigo y así nunca dejes de lucir tu colección
+                    en cualquier parte. Compra tu colección de Funko POP tv, Marvel,
+                    Disney, Anime y más en un solo lugar. Te mostraremos todas nuestras
+                    promociones.
                   </p>
                   <b-button
                     href="/promociones"
                     variant="primary"
-                    class="btn-funko-primary w-100 mb-5"
+                    class="btn-funko-primary w-100"
                     >Ver más</b-button
                   >
                 </div>
@@ -304,11 +278,11 @@ export default {
 }
 .exclusivos {
   justify-content: center;
-   margin: auto;
+  margin: auto;
 }
 .vendidos {
   justify-content: center;
-   margin: auto;
+  margin: auto;
 }
 .funko-home-sub-title {
   color: $celeste;
@@ -329,11 +303,6 @@ export default {
 p {
   text-align: justify;
   font-size: 800;
-}
-
-.funko-home-historia {
-  margin-left: 70px;
-  margin-right: 40px;
 }
 
 .funko-home-bienvenida {
