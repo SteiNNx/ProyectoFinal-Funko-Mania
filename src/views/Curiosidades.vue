31daf840b01cda6bef10cd16c6073e209775e3d5
Natalia <nataliagonzalez.1802@gmail.com>
2022-04-19 18:43:04 -0400

    cambios curiosidades

--- src/views/Curiosidades.vue
+++ src/views/Curiosidades.vue
@@ -1,32 +1,11 @@
 <template>
-  <div class="accordion" role="tablist">
-    <div class="container mt-5 mb-5">
-      <b-card no-body class="mb-1">
-        <b-card-header header-tag="header" class="p-1" role="tab">
-          <b-button
-            class="btn-funko-primary w-100"
-            block
-            v-b-toggle.accordion-1
-            variant="info"
-            >Sabias que...</b-button
-          >
-        </b-card-header>
-        <b-collapse
-          id="accordion-1"
-          visible
-          accordion="my-accordion"
-          role="tabpanel"
-        >
-          <b-card-body>
-            <b-card-text>{{ text }}</b-card-text>
-          </b-card-body>
-        </b-collapse>
-      </b-card>
+  <div class="container">
+    <b-card-group columns>
       <b-card
-        class="mt-5 img-curiosidades"
+        title="Tienen su propio juego de mesa"
         img-src="@/assets/img/funkoverse.png"
-        img-alt="funkoverse"
-        img-right
+        img-alt="Image"
+        img-top
       >
         <b-card-text>
           Tantas son las licencias que dispone que Funko ya se ha montado su
@@ -36,56 +15,85 @@
         </b-card-text>
       </b-card>
 
+      <b-card header="Donde nacen ?">
+        <blockquote class="blockquote mb-0">
+          <p>
+            Un 23 de octubre de 1998 se lanzó en Estados Unidos unas figuras
+            conocidas como Funko Pops.
+          </p>
+          <footer class="blockquote-footer">
+            pequeñas figuras cabezonas
+            <cite title="Source Title">del personaje que quieras</cite>
+          </footer>
+        </blockquote>
+      </b-card>
+
       <b-card
-        class="mt-5 img-curiosidades"
+        title="Pueden llegar a precios de 13.300 dólares"
         img-src="@/assets/img/caros.png"
-        img-alt="caros"
-        img-left
+        img-alt="Image"
+        img-top
       >
         <b-card-text>
-          El éxito de los Funko Pops fue tal que hoy día hay ciertas figuras que
-          valen una fortuna, ya sea porque se lanzaron pocas tiradas en su
-          momento o porque directamente se realizó una edición especial de la
-          misma. Dentro de los Funko Pops más exclusivos que son casi imposibles
-          de conseguir encontramos figuras como Loki, con un valor de más de
-          1.000 dólares, un Green Lantern que brilla en la oscuridad por un
-          valor de hasta 2.400 dólares o la joya de la corona, una figura del
-          personaje de La naranja mecánica Alex DeLarge que brilla en la
-          oscuridad, cuyo precio llega a los 13.300 dólares.
+          Hoy día hay ciertas figuras que valen una fortuna, ya sea porque se
+          lanzaron pocas tiradas en su momento o porque directamente se realizó
+          una edición especial de la misma. Dentro de los Funko Pops más
+          exclusivos que son casi imposibles de conseguir encontramos figuras
+          como Loki, con un valor de más de 1.000 dólares, un Green Lantern que
+          brilla en la oscuridad por un valor de hasta 2.400 dólares o la joya
+          de la corona, una figura del personaje de La naranja mecánica Alex
+          DeLarge que brilla en la oscuridad, cuyo precio llega a los 13.300
+          dólares.
+        </b-card-text>
+        <b-card-text class="small text-muted"
+          >Last updated 3 mins ago</b-card-text
+        >
+      </b-card>
+
+      <b-card bg-variant="primary" text-variant="white">
+        <blockquote class="card-blockquote">
+          <p>
+            Su edificio situado en Everett, Washington, tiene una tienda en la
+            planta baja de más de 5.000 metros cuadrados, con zonas temáticas
+            basadas en las franquicias más populares, como Star Wars o Harry
+            Potter.
+          </p>
+        </blockquote>
+      </b-card>
+
+      <b-card>
+        <b-card-title>Mike Becker creador funko pop</b-card-title>
+        <b-card-text>
+          los orígenes de los Funko Pops tienen lugar en la franquicia Bob's Big
+          Boy, una cadena de hamburguesas que tuvo un enorme éxito en Estados
+          Unidos.
+        </b-card-text>
+        <b-card-text class="small text-muted"
+          >compro la licencia de Bob y produjo su funko
         </b-card-text>
       </b-card>
-    </div>
+
+      <b-card
+        img-src="@/assets/img/funko1.png"
+        img-alt="funko1"
+        overlay
+      ></b-card>
+
+      <b-card img-src="@/assets/img/funko2.png" img-alt="Image" img-top>
+        <b-card-text>
+          No fue hasta que lanzaron las figuras de Juego de Tronos y The Walking
+          Dead cuando los fans ya empezaron a coger bastante cariño a las
+          figuras Funko Pop
+        </b-card-text>
+      </b-card>
+    </b-card-group>
   </div>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      text: `
-          Un 23 de octubre de 1998 se lanzó en Estados Unidos las figuras conocidas como Funko Pops y desde entonces nada sería lo mismo en todo el mundo. Estas pequeñas figuras cabezonas se caracterizan por representar personajes de todo tipo, desde personajes de cine y series hasta celebridades reales. Se estima que existen mas de 17 mil Figuras Pop en todo el mundo. La popularidad de estos muñecos va creciendo a medida que las series, películas o videojuegos se van poniendo de moda con sus nuevos estrenos..
-        `,
-    };
-  },
-};
-</script>
+<script></script>
 
 <style lang="scss" scoped>
 * {
   font-family: $akshar;
-}
-.principal {
-  background-image: "../assets/img/banner8.png";
-  background-repeat: no-repeat;
-}
-.funko-home-title {
-  text-transform: uppercase;
-  text-align: center;
-  color: $celeste;
-  font-family: $akshar;
-  font-size: 600;
-}
-.img-curiosidades {
-  height: 18rem;
 }
 </style>
